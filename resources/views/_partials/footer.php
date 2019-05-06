46b5e04bbc035d30ff7f05d59224478c5c1510d6
lat1992 <lat1992@users.noreply.github.com>
2019-05-06 18:34:25 +0200

    Update footer.php

--- resources/views/_partials/footer.php
+++ resources/views/_partials/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-    <p>Copyright &copy; 2016-2017 <a href="http://jitamin.com">Jitamin</a> Version <?= APP_VERSION ?>  All rights reserved.
-    <span class="pull-right">Jitamin on <a href="https://github.com/jitamin/jitamin" title="Jitamin is developed on Github"><i class="fa fa-github"></i> Github</a></span>
+    <p>Copyright &copy; 2016-2019 <a href="http://jitamin.com">Jitamin</a> Version <?= APP_VERSION ?>  All rights reserved.
+    <span class="pull-right">Jitamin on <a href="https://github.com/lat1992/jitamin" title="Jitamin is developed on Github"><i class="fa fa-github"></i> Github</a></span>
     </p>
 </div>
